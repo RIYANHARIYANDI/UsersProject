ec0264ee93e19e51295f900fb8b64a8e042a4789
RIYANHARIYANDI <riyanhariyandi@gmail.com>
2021-04-22 14:29:44 +0700

    UsersProject

---
+++ app/src/main/java/com/riyan/userproject/service/GetService.java
@@ -0,0 +1,12 @@
+package com.riyan.userproject.service;
+import com.riyan.userproject.model.User;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface GetService {
+    @GET("/users")
+    Call<List<User>> getUsersList();
+}
